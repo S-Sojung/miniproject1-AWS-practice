5c677838e9b1251b02a770ca3e1631e4357aa14b
unknown <deliah07@naver.com>
2023-02-28 20:18:53 +0900

    Company 회원가입, 로그인 구현 (최종)

--- src/main/java/shop/mtcoding/miniproject/controller/CompanyContoller.java
+++ src/main/java/shop/mtcoding/miniproject/controller/CompanyContoller.java
@@ -1,19 +1,33 @@
 package shop.mtcoding.miniproject.controller;
 
+import java.util.List;
+import java.util.StringTokenizer;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import shop.mtcoding.miniproject.dto.company.CompanyReq.JoinCompanyReqDto;
 import shop.mtcoding.miniproject.dto.company.CompanyReq.LoginCompanyReqDto;
+import shop.mtcoding.miniproject.dto.post.PostReq.PostSaveReqDto;
+import shop.mtcoding.miniproject.dto.post.PostResp.PostTitleRespDto;
 import shop.mtcoding.miniproject.handler.ex.CustomException;
+import shop.mtcoding.miniproject.model.Company;
+import shop.mtcoding.miniproject.model.CompanyRepository;
+import shop.mtcoding.miniproject.model.Post;
+import shop.mtcoding.miniproject.model.PostRespository;
+import shop.mtcoding.miniproject.model.Skill;
+import shop.mtcoding.miniproject.model.SkillRepository;
 import shop.mtcoding.miniproject.model.User;
 import shop.mtcoding.miniproject.model.UserRepository;
 import shop.mtcoding.miniproject.service.CompanyService;
+import shop.mtcoding.miniproject.service.PostService;
 
 @Controller
 public class CompanyContoller {
@@ -27,6 +41,18 @@ public class CompanyContoller {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRespository postRepository;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @Autowired
+    private PostService postService;
+
     // 인증에 필요한 일이기 때문에 company/login 이 아닌 이어서 했습니다.
     @GetMapping("/companyLoginForm")
     public String companyLoginForm() {
@@ -34,7 +60,7 @@ public class CompanyContoller {
     }
 
     @PostMapping("companyLogin")
-    public String personLogin(LoginCompanyReqDto loginCompanyReqDto) {
+    public String companyLogin(LoginCompanyReqDto loginCompanyReqDto) {
 
         if (loginCompanyReqDto.getEmail() == null ||
                 loginCompanyReqDto.getEmail().isEmpty()) {
@@ -130,20 +156,56 @@ public class CompanyContoller {
     }
 
     @GetMapping("/company/posts")
-    public String companyPosts() {
+    public String companyPosts(Model model) {
+        // companyMocLogin();
+
+        User userPS = (User) session.getAttribute("principal");
+        List<PostTitleRespDto> postTitleList = postRepository.findAllTitleByCInfoId(userPS.getCInfoId());
+        model.addAttribute("postTitleList", postTitleList);
         return "company/posts";
     }
 
-    @GetMapping("/company/postDetail")
-    public String personDetail() {
+    @GetMapping("/company/postDetail/{id}")
+    public String personDetail(@PathVariable int id, Model model) {
+        // companyMocLogin();
+
+        User userPS = (User) session.getAttribute("principal");
+        Post postPS = (Post) postRepository.findById(id);
+        Company companyPS = (Company) companyRepository.findById(userPS.getCInfoId());
+        Skill skillPS = (Skill) skillRepository.findByPostId(id);
+        StringTokenizer skills = new StringTokenizer(skillPS.getSkills(), ",");
+
+        if (postPS.getCInfoId() != userPS.getCInfoId()) {
+            throw new CustomException("게시글을 수정할 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+        // 공고 디테일 보기 //인증 및 권한체크
+        model.addAttribute("post", postPS);
+        model.addAttribute("company", companyPS);
+        model.addAttribute("skills", skills);
 
         return "company/postDetail";
     }
 
     @GetMapping("/company/savePostForm")
-    public String personSavePostForm() {
+    public String personSavePostForm(Model model) {
+        // companyMocLogin();
+        User userPS = (User) session.getAttribute("principal");
+        Company companyPS = (Company) companyRepository.findById(userPS.getCInfoId());
 
+        model.addAttribute("company", companyPS);
+        model.addAttribute("skills", Skill.madeSkills());
         return "company/savePostForm";
+    }
+
+    @PostMapping("/company/savePost")
+    public String personSavePost(Model model, PostSaveReqDto PostSaveReqDto) {
+        // companyMocLogin();
+        User userPS = (User) session.getAttribute("principal");
+
+        // postinsert skillinsert 동시 진행
+        int id = postService.공고등록(PostSaveReqDto, userPS.getCInfoId());
+
+        return "redirect:/company/postDetail/" + id; // +id
     }
 
 }
