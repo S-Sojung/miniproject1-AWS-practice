6970d03593e433014ef213de0195302321653cea
JungminK1m <deliah07@naver.com>
2023-03-06 02:48:18 +0900

    고객센터 컨트롤러

--- src/main/java/shop/mtcoding/miniproject/controller/IndexContoller.java
+++ src/main/java/shop/mtcoding/miniproject/controller/IndexContoller.java
@@ -1,16 +1,29 @@
 package shop.mtcoding.miniproject.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import shop.mtcoding.miniproject.dto.customerService.customerServiceResp.PersonCustomerServiceRespDto;
+import shop.mtcoding.miniproject.model.CompanyCustomerServiceRepository;
+import shop.mtcoding.miniproject.model.PersonCustomerServiceRepository;
 
 @Controller
 public class IndexContoller {
 
     @Autowired
     private HttpSession session;
+
+    @Autowired
+    private PersonCustomerServiceRepository personCustomerServiceRepository;
+
+    @Autowired
+    private CompanyCustomerServiceRepository companyCustomerServiceRepository;
 
     @GetMapping("/")
     public String main() {
@@ -24,7 +37,17 @@ public class IndexContoller {
     }
 
     @GetMapping("/customerService")
-    public String customerService() {
+    public String customerService(Model model) {
+
+        // 개인, 기업 질문 리스트 가져오기\
+        List<PersonCustomerServiceRespDto> personFAQ = (List<PersonCustomerServiceRespDto>) personCustomerServiceRepository.findAll();
+
+
+        // 모델에 담기
+        model.addAttribute("PersonFAQ", model)
+
+
+
         return "/customerService";
     }
 }
